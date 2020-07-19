a6b583dc298e4c41980a188ce55a8d6244060da2
Govind Patwal <67445971+Govind-Patwal@users.noreply.github.com>
2020-07-19 18:52:29 -0400

    some changes to files

--- Counitng Vowels/vowels.py
+++ Counitng Vowels/vowels.py
@@ -1,11 +1,16 @@
-#import pandas as pd
+# A small funcion that asks the user for a string and return the count of vowels in it
 
-count = 0
-string1 = input()
-for word in string1:
-    if word == "a" or word == "e" or word == "i" or word == "o" or word == "u":
-        count += 1
+# Initializing the vowel count to 0
+vowel_count = 0
 
-print(count)
+# Prompting the user for input
+print("Please enter your text below: ")
 
+# Store the inputted string
+user_input = input()
+for word in user_input:
+    if word in "aeiouAEIOU":
+        vowel_count += 1
 
+# Print
+print(f"The total number of vowels in the text that your entered are {vowel_count}.")
