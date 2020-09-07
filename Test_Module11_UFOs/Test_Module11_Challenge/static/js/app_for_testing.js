3447b66ede6905e21e55bcb5354cc1651a3964c3
Govind Patwal <67445971+Govind-Patwal@users.noreply.github.com>
2020-09-07 11:09:08 -0400

    some changes

--- Test_Module11_UFOs/Test_Module11_Challenge/static/js/app_for_testing.js
+++ Test_Module11_UFOs/Test_Module11_Challenge/static/js/app_for_testing.js
@@ -4,7 +4,7 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-
+// Funciton to build table
 function buildTable(data) {
     // code to clear existing data
     tbody.html("");
@@ -14,7 +14,6 @@ function buildTable(data) {
     data.forEach((dataRow) => {
          // Append a row to the table body
         let row = tbody.append("tr");
-
         // Loop through each field in the dataRow and add
         // each value as a table cell (td)
         Object.values(dataRow).forEach((val) => {
