f8366cf0324cafe58b7469a877a835ce65638905
Govind Patwal <67445971+Govind-Patwal@users.noreply.github.com>
2020-09-13 17:27:52 -0400

    some changes here

--- Test_Module_12/challenge12/plots.js
+++ Test_Module_12/challenge12/plots.js
@@ -27,15 +27,15 @@ function buildMetadata(sample) {
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
-    PANEL.append("h6").text(result.id);
-    PANEL.append("h6").text(result.ethnicity);
-    PANEL.append("h6").text(result.gender);
-    PANEL.append("h6").text(result.age);
-    PANEL.append("h6").text(result.location);
-    PANEL.append("h6").text(result.bbtype);
-    PANEL.append("h6").text(result.wfreq);
+    PANEL.append("h6").text("ID: " + result.id);
+    PANEL.append("h6").text("ETHINICTY: " + result.ethnicity);
+    PANEL.append("h6").text("GENDER: " + result.gender);
+    PANEL.append("h6").text("AGE: " + result.age);
+    PANEL.append("h6").text("LOCATION: " + result.location);
+    PANEL.append("h6").text("BBTYPE: " + result.bbtype);
+    PANEL.append("h6").text("WREF: " + result.wfreq);
 
   });
 }
 
-  init();
+init();
