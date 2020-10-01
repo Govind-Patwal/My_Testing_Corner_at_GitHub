d71b4a8f2f692c13bd3ab40cfa78ad4eecc577bf
Govind Patwal <67445971+Govind-Patwal@users.noreply.github.com>
2020-10-01 07:38:49 -0400

    some changes

--- Test_Module15/R_Analysis/01_Demo/Demo.R
+++ Test_Module15/R_Analysis/01_Demo/Demo.R
@@ -2,8 +2,12 @@ x <- 3
 
 4 -> y
 x <- 5
-x -> 10
 numlist <- c(0,1,2,3,4,5,6,7,8,9,10)
 numlist <- c(0,1,2,3,4,5,6,7,8,9)
 demo_table <- read.csv(file='demo.csv',check.names=F,stringsAsFactors = F)
 demo_table2 <- fromJSON(txt='demo.json')
+demo_table3 <- read.csv('demo2.csv', check.names = F, stringsAsFactors = F)
+demo_table7 <- read.xlsx('Vehicle_Data.xlsx', check.names = F, stringsAsFactors = F)
+demo_table7 <- read.csv('Vehicle_Data.xlsx', check.names = F, stringsAsFactors = F)
+long_table <- gather(demo_table3,key="Metric",value="Score",buying_price:popularity)
+long_table <- demo_table3 %>% gather(key="Metric",value="Score",buying_price:popularity)
