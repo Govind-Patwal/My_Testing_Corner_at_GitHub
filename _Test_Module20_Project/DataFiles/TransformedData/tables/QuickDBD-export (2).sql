dd90de24669289572c0a988e16da64e26a1a7eff
unknown <gspatwal81@gmail.com>
2020-11-15 14:58:28 -0500

    some changes

---
+++ _Test_Module20_Project/DataFiles/TransformedData/tables/QuickDBD-export (2).sql
@@ -0,0 +1,12 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Hotel_Review" (
+    "Review" varchar   NOT NULL,
+    "Reviewer_Sentiment" varcahr   NOT NULL,
+    CONSTRAINT "pk_Hotel_Review" PRIMARY KEY (
+        "Review"
+     )
+);
+
