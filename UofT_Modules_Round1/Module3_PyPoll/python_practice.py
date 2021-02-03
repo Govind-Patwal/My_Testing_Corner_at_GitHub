29db291ceee08f028e002d898097b3292ae5bb60
unknown <gspatwal81@gmail.com>
2021-02-03 09:02:17 -0500

    modified python.practice.py

--- UofT_Modules_Round1/Module3_PyPoll/python_practice.py
+++ UofT_Modules_Round1/Module3_PyPoll/python_practice.py
@@ -1,1 +1,6 @@
-print("Hello world")
+def print_string(x):
+    print(f'Hello {x}! hope youa are doing well, this is the computer program saying hello to you')
+
+print("What do you want printed?")
+y = input()    
+print_string(y)
