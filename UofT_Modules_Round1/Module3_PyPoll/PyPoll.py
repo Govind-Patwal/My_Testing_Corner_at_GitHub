1c4d56d224e15849bb1fc0f1237c3dcd887a7a4f
unknown <gspatwal81@gmail.com>
2021-02-07 15:26:59 -0500

    modified python.practice.py

--- UofT_Modules_Round1/Module3_PyPoll/PyPoll.py
+++ UofT_Modules_Round1/Module3_PyPoll/PyPoll.py
@@ -1,17 +1,38 @@
 #1 adding dependencies
 import csv
 
+# Initialize the total vote counter
+total_votes = 0
+candidate_list = []
+candidate_vote = {}
 # open the csv
 with open('Resources/election_results.csv') as election_data:
     # read the csv
     file_reader = csv.reader(election_data)
 
+    # read the header row
     headers = next(file_reader)
-    print(headers)
 
-    for 
+    for rows in file_reader:
+        #code to find the total vote count
+        total_votes +=1
 
-#2 the number of candidates
+        # candidate name
+        candidate = rows[2]
+
+        # code to find all unique candidates
+        if rows[2] not in candidate_list:
+            candidate_list.append(candidate) 
+            candidate_vote[candidate] = 1
+        else:
+            candidate_vote[candidate] += 1
+
+        # else:
+        #     candidate_vote[rows[2]] +=1     
+
+print(candidate_list)
+print(candidate_vote)
+#2 the number of  candidates
 
 #3 the number of votes received by each candidate
 
--- UofT_Modules_Round1/Module3_PyPoll/python_practice.py
+++ UofT_Modules_Round1/Module3_PyPoll/python_practice.py
@@ -1,13 +1,2 @@
-# https://bootcampspot.instructure.com/courses/193/pages/3-dot-3-1-import-and-inspect-the-data?module_item_id=56598
-
-
-# read the data file
-
-# find the total number of votes
-
-# find the candidates who received votes
-
-# total number of votes each candidate won
-
-# winner of the election and the %
-
+list = [1,3,4,4,4,4,4]
+print(list.index(1))
