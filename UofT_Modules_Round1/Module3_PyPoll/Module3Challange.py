3481d2427fc10b47193647505581fd841c78636b
unknown <gspatwal81@gmail.com>
2021-02-07 20:38:03 -0500

    modified python.practice.py

--- UofT_Modules_Round1/Module3_PyPoll/Module3Challange.py
+++ UofT_Modules_Round1/Module3_PyPoll/Module3Challange.py
@@ -2,6 +2,34 @@
 import csv
 
 #reading in the file
-with open('/Resources/election_results.csv') as election_data:
-    
+with open('Resources/election_results.csv') as election_data:
+    data_for_file = csv.reader(election_data)
+
+    headers = next(data_for_file)
+    print(headers)
+
+    voter_count = 0
+    county = []
+    county_votes = {}
+
+    for rows in data_for_file:
+        # counter for total number of votes
+        voter_count += 1
+
+        # declaring a variable for city
+        x = rows[1]
+
+        # creating a list of all county names
+        if x not in county:
+            county.append(x)
+            county_votes[x] = 1
+        else:
+            county_votes[x] += 1  
+
+print(voter_count)       
+print(county)
+print(county_votes)
+
+        
+        
 
