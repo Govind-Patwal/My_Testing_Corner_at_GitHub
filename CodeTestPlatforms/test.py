89e6c529757cb3d4881dbcbd89183c78a96cc716
unknown <gspatwal81@gmail.com>
2021-02-15 12:14:10 -0500

    modified python.practice.py

--- CodeTestPlatforms/test.py
+++ CodeTestPlatforms/test.py
@@ -1,28 +1,11 @@
-# pseudcode
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+for planet in planets:
+    print(planet, end=' ') # print all on same line
 
-# create a list of planets
+print('\n')
 
-list1 = [i for i in range(10, 21)]
-# print(list1)
-
-# # print('first and last six elements', list1[:6], list1[-6:])
-# # print('first 6 ', list1[:6], list1[-6:])
-
-# print(f'the first element is {list1[0]}')
-# print(f'the last element is {list1[-1]}')
-# print(f'the first 5 elements are {list1[:5]}')
-# print(f'the last 5 elements are {list1[-5:]}')
-# print(f'elements 2 to 5 are {list1[1:5]}') # just like range(), the iteration end in the second last index
-
-# print(f'elements 2 to end of list are {list1[1:]}')
-# print(f'all elements - method 1 {list1[:]}')
-# print(f'all elements - method 2 {list1}')
-# print(f'all elements except the first and last {list1[1:-1]}')
-
-list1.pop()
-
-print(help(list1))
-
+for planet in range(len(planets)):
+    print(planets[planet], end=' ') # print all on same line
 
 
 
