6e9ac880e0e5dca16081452f63922e5fbc27736e
unknown <gspatwal81@gmail.com>
2021-02-15 10:20:03 -0500

    modified python.practice.py

--- CodeTestPlatforms/test.py
+++ CodeTestPlatforms/test.py
@@ -1,11 +1,24 @@
-for i in [1,2,3,4,0,0,0,2]:
-    if i:
-        print('True')
-    else:
-        print('False')    
+# pseudcode
+
+# create a list of planets
+
+list1 = [i for i in range(10, 21)]
+print(list1)
+
+# print('first and last six elements', list1[:6], list1[-6:])
+# print('first 6 ', list1[:6], list1[-6:])
+
+print(f'the first element is {list1[0]}')
+print(f'the last element is {list1[-1]}')
+print(f'the first 5 elements are {list1[:5]}')
+print(f'the last 5 elements are {list1[-5:]}')
+print(f'elements 2 to 5 are {list1[1:5]}')
+print(f'elements 2 to end of list are {list1[1:]}')
+print(f'all elements - method 1 {list1[:]}')
+print(f'all elements - method 2 {list1}')
 
 
-print(20/3)
-print(20//3)
-print(int(20/3))
-print(round(20/3))
+
+
+
+
