f547288f29da8491f33298d1b5c015b00b4de0e5
unknown <gspatwal81@gmail.com>
2021-03-22 08:43:41 -0400

    changed MissionDataJobStart.md

--- Python3Specialization-UnivMichigan-Coursera/practice.py
+++ Python3Specialization-UnivMichigan-Coursera/practice.py
@@ -1,1 +1,29 @@
-print('my daughter has gotten up')
+# cls
+
+# input in python
+# from the user
+# var = input()
+
+# assigning to variables
+# var1 = 34
+
+# loading a file - excel, csv, tsv, zip, pdf, word
+# images
+
+# output
+# on monitor - text, image, table
+# as an input to something else
+
+# for character in 'cool string':
+#     print(character)
+
+# While can be used if you want to stop something when the user is going to stop something
+
+list1 = []
+item = ''
+while item != 'done':
+    print('Choose an item:' )
+    item = input()
+    if item != 'done':
+        list.append(item)
+print(list1)
