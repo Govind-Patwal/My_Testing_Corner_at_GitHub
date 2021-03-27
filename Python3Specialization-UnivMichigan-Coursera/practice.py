ccbe0cc60c672159d817afad198657c4c1b9470e
unknown <gspatwal81@gmail.com>
2021-03-27 04:58:04 -0400

    modified MissionDataJobStart.py

--- Python3Specialization-UnivMichigan-Coursera/practice.py
+++ Python3Specialization-UnivMichigan-Coursera/practice.py
@@ -28,5 +28,44 @@
 #     val = val + "!"
 #     print(
 
- ====== conditional execution ==============
- finished - https://www.coursera.org/learn/python-basics/lecture/8Kach/conditional-execution
+# ====== conditional execution ==============
+ # finished - https://www.coursera.org/learn/python-basics/lecture/8Kach/conditional-execution
+
+
+ # REvision of yesterday
+
+values and data types
+int, float, str
+int(). float(), str()
+
+type(1), type(1.1), type('1')
+
+operators
+() *** x /  //(integer division/mod/modulus)  % (reminder)  +  -   
+
+variables can start a....z, A....Z, no special except _, cannot start with a number  _ used in loops __ special
+
+reassignmentuusing = , the RHS is assigned to the LHS and not vice versa
+
+update - 
+
+refrain from hard code
+
+turtle - a library native to python to draw graphics
+
+import turtle
+window = python.Screen()
+alex = turtle.Turtle()
+alex.forward(50)
+alex.left(30)
+alex(forward(-30))
+alex.left(-30)
+
+alex.up()
+alex.down()
+alex.stamp
+
+
+
+
+
