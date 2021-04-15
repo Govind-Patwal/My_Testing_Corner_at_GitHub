6e44eb78f67122d9c4c5b38e0ca1de6bd4f76a49
unknown <gspatwal81@gmail.com>
2021-04-15 06:28:43 -0400

    modified MissionDataJobStart.py

--- Python3Specialization-UnivMichigan-Coursera/Module4_Classes/pypractice.py
+++ Python3Specialization-UnivMichigan-Coursera/Module4_Classes/pypractice.py
@@ -429,94 +429,94 @@
 # print(b1.name)
 # print(b1.hunger)
 
-class Pokemon():
-    attack = 12
-    defense = 10
-    health = 15
-    p_type = "Normal"
+# class Pokemon():
+#     attack = 12
+#     defense = 10
+#     health = 15
+#     p_type = "Normal"
 
-    def __init__(self, name, level = 5):
-        self.name = name
-        self.level = level
+#     def __init__(self, name, level = 5):
+#         self.name = name
+#         self.level = level
 
-    def train(self):
-        self.update()
-        '''
-        def update(self):
-            self.health_boost = 5
-            self.attack_boost = 3
-            self.defense_boost = 2
-            self.evolve = 10
-        '''
-        self.attack_up()
-        self.defense_up()
-        self.health_up()
-        self.level = self.level + 1
-        if self.level%self.evolve == 0:
-            return self.level, "Evolved!"
-        else:
-            return self.level
+#     def train(self):
+#         self.update()
+#         '''
+#         def update(self):
+#             self.health_boost = 5
+#             self.attack_boost = 3
+#             self.defense_boost = 2
+#             self.evolve = 10
+#         '''
+#         self.attack_up()
+#         self.defense_up()
+#         self.health_up()
+#         self.level = self.level + 1
+#         if self.level%self.evolve == 0:
+#             return self.level, "Evolved!"
+#         else:
+#             return self.level
 
-    def attack_up(self):
-        self.attack = self.attack + self.attack_boost
-        return self.attack
+#     def attack_up(self):
+#         self.attack = self.attack + self.attack_boost
+#         return self.attack
 
-    def defense_up(self):
-        self.defense = self.defense + self.defense_boost
-        return self.defense
+#     def defense_up(self):
+#         self.defense = self.defense + self.defense_boost
+#         return self.defense
 
-    def health_up(self):
-        self.health = self.health + self.health_boost
-        return self.health
+#     def health_up(self):
+#         self.health = self.health + self.health_boost
+#         return self.health
 
-    def update(self):
-        self.health_boost = 5
-        self.attack_boost = 3
-        self.defense_boost = 2
-        self.evolve = 10
+#     def update(self):
+#         self.health_boost = 5
+#         self.attack_boost = 3
+#         self.defense_boost = 2
+#         self.evolve = 10
 
-    def __str__(self):
-        self.update()
-        return "Pokemon name: {}, Type: {}, Level: {}".format(self.name, self.p_type, self.level)
+#     def __str__(self):
+#         self.update()
+#         return "Pokemon name: {}, Type: {}, Level: {}".format(self.name, self.p_type, self.level)
 
-class Grass_Pokemon(Pokemon):
-    attack = 15
-    defense = 14
-    health = 12
+# class Grass_Pokemon(Pokemon):
+#     attack = 15
+#     defense = 14
+#     health = 12
 
-    def update(self):
-        self.health_boost = 6
-        self.attack_boost = 2
-        self.defense_boost = 3
-        self.evolve = 12
+#     def update(self):
+#         self.health_boost = 6
+#         self.attack_boost = 2
+#         self.defense_boost = 3
+#         self.evolve = 12
 
-    def moves(self):
-        self.p_moves = ["razor leaf", "synthesis", "petal dance"]
+#     def moves(self):
+#         self.p_moves = ["razor leaf", "synthesis", "petal dance"]
 
-    def action(self):
-        return '{} knows a lot of different moves!'.format(self.name)
+#     def action(self):
+#         return '{} knows a lot of different moves!'.format(self.name)
 
-    def train(self):
-        self.update()
-        self.defense_up()
-        self.health_up()
-        self.level = self.level + 1
-        if self.level >= 10:
-            self.attack_up()
-        if self.level%self.evolve == 0:
-            return self.level, "Evolved!"
-        else:
-            return self.level     
+#     def train(self):
+#         self.update()
+#         self.defense_up()
+#         self.health_up()
+#         self.level = self.level + 1
+#         if self.level >= 10:
+#             self.attack_up()
+#         if self.level%self.evolve == 0:
+#             return self.level, "Evolved!"
+#         else:
+#             return self.level     
 
-p1 = Grass_Pokemon('Belle')
+# p1 = Grass_Pokemon('Belle')
 
-for i in range(10):
-    print('--------------')
-    print('level: {}'.format(p1.level))
-    print('attack: {}'.format(p1.attack))
-    print(" *** training *** ")
-    p1.train()
+# for i in range(10):
+#     print('--------------')
+#     print('level: {}'.format(p1.level))
+#     print('attack: {}'.format(p1.attack))
+#     print(" *** training *** ")
+#     p1.train()
 
-p1.attack_up()    
+# p1.attack_up()    
 
 
