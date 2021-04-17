a342aa70b610a9f46fb3fe905ba65279fef7b561
unknown <gspatwal81@gmail.com>
2021-04-17 05:33:19 -0400

    modified MissionDataJobStart.py

--- Python3Specialization-UnivMichigan-Coursera/Module4_Classes/pypractice.py
+++ Python3Specialization-UnivMichigan-Coursera/Module4_Classes/pypractice.py
@@ -562,9 +562,22 @@
 
 # print('this is after the assert')
 
-import unittest
+import pytest
 
-assert 1 == 1 
+def round6(num):
+    return int(num + .6)
+
+# ---- automated unit test ----
+
+def test_round6():
+    assert round6(9.7) == 10
+    assert round6(8.5) == 8
+
+# ----- main program follows -----
+
+if __name__ == '__main__':
+    num = float(input('Enter a decimal: '))
+    print('The value rounded is: ' + str(round6(num)))
 
 
 
