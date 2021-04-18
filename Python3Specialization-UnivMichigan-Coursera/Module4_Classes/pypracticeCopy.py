0feb4f3a1ca7e15986b9f5a071cdf518d248675d
unknown <gspatwal81@gmail.com>
2021-04-18 09:27:28 -0400

    modified MissionDataJobStart.py

--- Python3Specialization-UnivMichigan-Coursera/Module4_Classes/pypracticeCopy.py
+++ Python3Specialization-UnivMichigan-Coursera/Module4_Classes/pypracticeCopy.py
@@ -3,8 +3,19 @@ This Course is one of the main reason I joined this specialization.
 It provides an intro to Classes and Inheretance.
 '''
 
-import pypractice
+# import moudules
 
-pypractice.main()
+# define functions and classes
+
+# define the main function that has the variables to be run for this program
+def main ():
+
+
+# include a statement that makes sure that any file that imported this does not
+# run the whole code
+
+if __name__ == '__main__':
+    main()
+
 
     
