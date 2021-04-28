1bbd1648d02575cf106d3fe3434b9e42e398f015
unknown <gspatwal81@gmail.com>
2021-04-28 08:01:37 -0400

    modified MissionDataJobStart.py

--- Python3Specialization-UnivMichigan-Coursera/Module5/pypractice.py
+++ Python3Specialization-UnivMichigan-Coursera/Module5/pypractice.py
@@ -1,19 +1,3 @@
-def my_logger(fn_to_be_decorated):
-    import logging
-    logging.basicConfig(filename='{}.log'.format(fn_to_be_decorated.__name__), level=logging.INFO)
-
-    def wrapper(*args, **kwargs):
-        logging.info('Ran with args: {}, and kwargs: {}'.format(args,kwargs))
-        return fn_to_be_decorated(*args, **kwargs)
-
-    return wrapper
-
-@my_logger
-def display_info(*args, **kwargs):
-    print('display_info ran with arguments ({}, {})'.format(args[0], args[1]))
-
-display_info('John', 25)
-
-@mylogger is a decorator function
-
-decorator can be implemented as a function or a class 
+my_string = 'Govind Patwal'
+print(my_string.replace('Gov', 'Disney'))
+new_string = 'Govind Patwal' + 'Gov'
