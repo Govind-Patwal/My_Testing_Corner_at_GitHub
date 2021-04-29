c4b35c4d6facec86af2a4fffa72ceb51720c2893
unknown <gspatwal81@gmail.com>
2021-04-29 06:51:05 -0400

    modified MissionDataJobStart.py

--- Python3Specialization-UnivMichigan-Coursera/Module5/pypractice.py
+++ Python3Specialization-UnivMichigan-Coursera/Module5/pypractice.py
@@ -1,15 +1,8 @@
-def foo(var1, var2):
-    # crating a copy of variables to changing __main__ variables
-    var1 = var1.copy()
-    print('List before mutation inside the function: {}'.format(var1))
-    # print(id(var1))
-    var1.append(89)
-    print('List after mutation inside the function: {}'.format(var1))
+import functools
 
-x,y = [21, 23],45
-print('List in __main__ before function call: {}'.format(x))
-
-# print('Id of original x: {}'.format( id(x)) ) 
-foo(x,y)
-
-print('List in __main__ after function call: {}'.format(x))
+list1 = ['this', 'is', 'the', 'thing']
+str1 = ' '.join(list1)
+print(str1)
+print('----------------')
+str2 = functools.reduce(lambda x,y: x+y, list1)
+print(str1)
