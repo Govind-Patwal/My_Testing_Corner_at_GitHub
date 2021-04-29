d25c57289911c51f439fc7411cb5d621c84b782d
unknown <gspatwal81@gmail.com>
2021-04-29 07:18:16 -0400

    modified MissionDataJobStart.py

--- Python3Specialization-UnivMichigan-Coursera/Module5/pypractice.py
+++ Python3Specialization-UnivMichigan-Coursera/Module5/pypractice.py
@@ -1,8 +1,3 @@
-import functools
+list1 = []
 
-list1 = ['this', 'is', 'the', 'thing']
-str1 = ' '.join(list1)
-print(str1)
-print('----------------')
-str2 = functools.reduce(lambda x,y: x+y, list1)
-print(str1)
+new = list1.append(23)
